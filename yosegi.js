2ad2c3cf6a8db98318549a011906b71fafa7dae8
Ryota Shioya <shioya@nuee.nagoya-u.ac.jp>
2016-12-24 18:00:48 +0900

    インテリセンスが効くようにコードを修正

--- yosegi.js
+++ yosegi.js
@@ -1,69 +1,75 @@
-var yosegi = {};
+var yosegi = {
 
-// レンダラで実行
-yosegi.getFileList = function(path, callback) {
-    var context = {
-        count: 0,
-        callback: callback,
-        tree: {},
-        finish: false,
-        searching: 0,
-        searchingDir: 1,
-    };
+    // レンダラで実行
+    getFileList: function(path, callback) {
+        var context = {
+            count: 0,
+            callback: callback,
+            tree: {},
+            finish: false,
+            searching: 0,
+            searchingDir: 1,
+        };
 
-    var remote = require("electron").remote.require("./yosegi");
-    //var yosegi = require("./yosegi");
-    
-    remote.getFileListBody(path, context, context.tree);
-}
+        context.func = function(val) {};
 
-// リモートで実行
-yosegi.getFileListBody = function(path, context, parent) {
-    //var fs = require("electron").remote.require("fs");
-    var fs = require("fs");
-
-    fs.readdir(path, function(err, files) {
-        if (err) {
-            //console.log(err);
-            return;
-        }
+        // server プロセスで getFileListBody を呼び出す
+        var remote = require("electron").remote.require("./yosegi");
+        //var yosegi = require("./yosegi");
         
-        context.searching += files.length;
-        context.searchingDir -= 1;
+        remote.getFileListBody(path, context, context.tree);
+    },
 
-        files.forEach(function(val, i) {
-            var filePath = path + "/" + val;
+    // リモートで実行
+    getFileListBody: function(path, context, parent) {
+        //var fs = require("electron").remote.require("fs");
+        var fs = require("fs");
 
-            fs.stat(filePath, function(err, stat) {
+        fs.readdir(path, function(err, files) {
+            if (err) {
+                //console.log(err);
+                return;
+            }
+            
+            context.searching += files.length;
+            context.searchingDir -= 1;
 
-                if (err) {
-                    //console.log(err);
-                }
-                else{
-                    // ファイル情報ノード作成
-                    var node = {
-                        size: stat.size,
-                        isDirectory: stat.isDirectory(),
-                        children: null
-                    };
-                    parent[filePath] = node;
+            files.forEach(function(val, i) {
+                var filePath = path + "/" + val;
 
-                    if (stat.isDirectory()) {
-                        node.children = {};
-                        context.searchingDir++;
-                        yosegi.getFileListBody(filePath, context, node.children);
+                fs.stat(filePath, function(err, stat) {
+
+                    if (err) {
+                        //console.log(err);
                     }
-                }
+                    else{
+                        // ファイル情報ノード作成
+                        var node = {
+                            size: stat.size,
+                            isDirectory: stat.isDirectory(),
+                            children: null
+                        };
+                        parent[filePath] = node;
 
-                context.count++;
-                context.searching -= 1;
-                if (context.searching == 0 && context.searchingDir == 0) {
-                    context.callback(context);
-                }
+                        if (stat.isDirectory()) {
+                            node.children = {};
+                            context.searchingDir++;
+                            yosegi.getFileListBody(filePath, context, node.children);
+                        }
+                    }
+
+                    context.count++;
+                    context.searching -= 1;
+                    if (context.searching == 0 && context.searchingDir == 0) {
+                        context.finish = false;
+                        context.callback(context);
+                    }
+                });
             });
-        });
 
-    });
-};
+        });
+    }
+};  // var yosegi = {}
+
 
 module.exports = yosegi;
