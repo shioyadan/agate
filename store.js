ef47d83445d3f0cc1c401542c4c7735fb90bb756
Ryota Shioya <shioya@ci.i.u-tokyo.ac.jp>
2020-12-27 16:18:00 +0900

    refactor: use "class" in Store implementation

--- store.js
+++ store.js
@@ -10,56 +10,93 @@ const ACTION = {
     FOLDER_OPEN: 8,
 };
 
-function Store(){
-    /* globals riot FileInfo */
-    riot.observable(this);
+const CHANGE = {
+};
 
-    let self = this;
 
-    self.tree = null;
-    self.treeFolderName = "";
-    self.treeLoadState = null;
-    self.treeCurrentLoadingFileName = "";
+class Store {
+    constructor() {
+        let self = this;
+        this.handlers_ = {};
 
-    self.width = 0;
-    self.height = 0;
+        this.tree = null;
+        this.treeFolderName = "";
+        this.treeLoadState = null;
+        this.treeCurrentLoadingFileName = "";
 
-    self.pointedPath = "";
-    self.pointedFileNode = null;
+        this.width = 0;
+        this.height = 0;
 
-    self.fileInfo_ = new FileInfo();
+        this.pointedPath = "";
+        this.pointedFileNode = null;
 
-    self.on(ACTION.TREE_LOAD, function(folderName) {
-        self.treeFolderName = folderName;
+        /* globals riot FileInfo */
+        this.fileInfo_ = new FileInfo();
 
-        self.fileInfo_.getFileTree(
-            folderName,
-            function(state, tree) {
-                // 読み込み終了
-                self.tree = tree;
-                self.treeLoadState = state;
-                self.trigger(ACTION.TREE_LOADED, self);       
-            },
-            function(state, filePath) {
-                // 読み込み状態の更新
-                self.treeLoadState = state;
-                self.treeCurrentLoadingFileName = filePath;
-                self.trigger(ACTION.TREE_LOADING, self);       
+        this.on(ACTION.TREE_LOAD, (folderName) => {
+            this.treeFolderName = folderName;
+
+            this.fileInfo_.getFileTree(
+                folderName,
+                (state, tree) => {
+                    // 読み込み終了
+                    this.tree = tree;
+                    this.treeLoadState = state;
+                    this.trigger(ACTION.TREE_LOADED, this);       
+                },
+                (state, filePath)  => {
+                    // 読み込み状態の更新
+                    this.treeLoadState = state;
+                    this.treeCurrentLoadingFileName = filePath;
+                    this.trigger(ACTION.TREE_LOADING, this);       
+                }
+            );
+        });
+
+        this.on(ACTION.CANVAS_RESIZED, (width, height) => {
+            this.width = width;
+            this.height = height;
+        });
+
+        this.on(ACTION.CANVAS_POINTER_CHANGE, (path, fileNode) => {
+            this.pointedPath = path;
+            this.pointedFileNode = fileNode;
+            this.trigger(ACTION.CANVAS_POINTER_CHANGED, this);       
+        });
+
+    }
+
+    on(event, handler) {
+        if (!event) {
+            console.log(`Unknown event ${event}`);            
+        }
+        if (!(event in this.handlers_ )) {
+            this.handlers_[event] = [];
+        }
+        this.handlers_[event].push(handler);
+    }
+
+    trigger(event, ...args) {
+        if (!event) {
+            console.log(`Unknown event ${event}`);            
+        }
+        if (event in this.handlers_) {
+            let handlers = this.handlers_[event];
+            for (let h of handlers) {
+                h.apply(null, args);
             }
-        );
-    });
+        }
+    }
 
-    self.on(ACTION.CANVAS_RESIZED, function(width, height){
-        self.width = width;
-        self.height = height;
-    });
-
-    self.on(ACTION.CANVAS_POINTER_CHANGE, function(path, fileNode){
-        self.pointedPath = path;
-        self.pointedFileNode = fileNode;
-        self.trigger(ACTION.CANVAS_POINTER_CHANGED, self);       
-    });
+    get ACTION(){
+        return ACTION;
+    }
+    get CHANGE(){
+        return CHANGE;
+    }
 }
 
 
-module.exports = Store;
+module.exports.Store = Store;
+module.exports.ACTION = ACTION;
+module.exports.CHANGE = CHANGE;
